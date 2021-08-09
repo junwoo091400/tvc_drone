67ee6fb3470ac44f4a6037c452c4615d2c15ec75
Raphli <raphael.secondaire@gmail.com>
2021-08-09 09:52:35 +0200

    cpu core allocation
    Former-commit-id: a3bc7918b9ebf446eaaace5a5a3eb8625028d011

--- drone_utils/bash_scripts/remote_drone_launch.sh
+++ drone_utils/bash_scripts/remote_drone_launch.sh
@@ -11,6 +11,10 @@ if [[ $1 == "c" ]]; then
     ssh drone@ert.local 'source /home/drone/drone_ws/devel/setup.bash;cd ~/drone_ws;catkin_make'
 fi
 
+# (ssh drone@ert.local 'sudo cpufreq-set -g performance')&
+(ssh drone@ert.local 'isolcpus=3')&
+
+
 export ROS_IP=$(hostname -I | cut -d' ' -f1) #automatically set to local ip address
 export ROS_MASTER_URI=http://$(getent hosts ert.local | awk '{ print $1 }'):11311 #automatically set to raspberry pi ip address
 
--- drone_utils/raspberrypi_interface/raspberrypi_files/remote_env_loader.sh
+++ drone_utils/raspberrypi_interface/raspberrypi_files/remote_env_loader.sh
@@ -2,7 +2,7 @@
 
 export ROS_IP=$(hostname -I | cut -d' ' -f1) #automatically set to local ip address
 export ROS_MASTER_URI=http://$(hostname -I | cut -d' ' -f1):11311 #automatically set to local ip address
-#export ROSLAUNCH_SSH_UNKNOWN=1
+export ROSLAUNCH_SSH_UNKNOWN=1
 
 source /home/drone/drone_ws/devel/setup.bash
 
