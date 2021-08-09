e4f722cbafef5c15c4c31060146f7c8d087d649b
Raphli <raphael.secondaire@gmail.com>
2021-08-09 18:16:20 +0200

    fix raspberry pi orientation
    Former-commit-id: 8ae45222bc3a25f1dc88ac6e710d63e09cb1a474

--- drone_gnc/scripts/avionic_bridge_node.py
+++ drone_gnc/scripts/avionic_bridge_node.py
@@ -23,8 +23,8 @@ def control_callback(control):
     bottom_motor_cmd = min(max(bottom_motor_cmd, 0), 80)
 
     # convert to PWM
-    servo1_DC = ((control.servo1/(np.pi/2)) + 1.27)*1000
-    servo2_DC = ((-control.servo2/(np.pi/2)) + 1.455)*1000
+    servo1_DC = ((-control.servo1/(np.pi/2)) + 1.455)*1000
+    servo2_DC = ((-control.servo2/(np.pi/2)) + 1.27)*1000
 
     top_motor_DC = int(top_motor_cmd*10 + 1000)
     bottom_motor_DC = int(bottom_motor_cmd*10 + 1000)
