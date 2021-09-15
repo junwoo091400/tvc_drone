3c713ba33eb04730cb2ceffaa1dd6b13ffc726c6
Raphlins <raphael.secondaire@gmail.com>
2021-09-15 09:28:21 +0200

    updated license 2
    Former-commit-id: b732aad5330e857e24e42fee12c096341dd7a191

--- drone_gnc/src/guidance/guidance_mpc/drone_guidance_mpc.cpp
+++ drone_gnc/src/guidance/guidance_mpc/drone_guidance_mpc.cpp
@@ -2,9 +2,9 @@
  *
  * Copyright (C) 2021  Raphaël Linsen
  *
- * This source code is subject to the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3. If a copy of the GNU General Public License was not distributed
- * with this file, you can obtain one at http://www.gnu.org/licenses/.
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
 
 #include "drone_guidance_mpc.h"
