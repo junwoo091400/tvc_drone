ffbcd31515a74639fc70010cd74807d0529246b4
Raphlins <raphael.secondaire@gmail.com>
2021-09-17 13:24:19 +0200

    deleted unused file + updated package info
    Former-commit-id: aba222656e629fc1e987979097f5fc4103ed58bc

--- drone_gnc/src/utils/control_devel.cpp
+++ drone_gnc/src/utils/control_devel.cpp
@@ -14,7 +14,6 @@
 
 #include "Eigen/Core"
 #include <drone_model.hpp>
-#include "mpc_utils.hpp"
 
 int main(int argc, char **argv) {
     // Init ROS time keeper node
@@ -22,16 +21,16 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh("util");
     Drone drone(nh);
 
-    Matrix<double, 11, 1> Q;
-    Matrix<double, Drone::NU, 1> R;
-    Q << 1, 1, 5,
-            0.1, 0.1, 0.5,
-            2, 2,
-            2, 2, 5;
-    R << 5, 5, 0.01, 0.01;
+//    Matrix<double, 11, 1> Q;
+//    Matrix<double, Drone::NU, 1> R;
+//    Q << 1, 1, 5,
+//            0.1, 0.1, 0.5,
+//            2, 2,
+//            2, 2, 5;
+//    R << 5, 5, 0.01, 0.01;
+//
+//    Matrix<double, NX - 2, NX - 2> QN;
+//    compute_LQR_terminal_cost(drone, Q, R, QN);
 
-    Matrix<double, NX - 2, NX - 2> QN;
-    compute_LQR_terminal_cost(drone, Q, R, QN);
-
-    ROS_INFO_STREAM("QN\n" << QN);
+//    ROS_INFO_STREAM("QN\n" << QN);
 }
