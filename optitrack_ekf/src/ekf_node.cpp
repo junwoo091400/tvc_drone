10c073c24aafbffaf468114e088b7b9bb946f7a1
Raphlins <raphael.secondaire@gmail.com>
2022-04-10 11:52:23 +0200

    Add separate optitrack EKF
    Former-commit-id: a223847c889b3f09abf2e115b7a4a3cab1b5af50

---
+++ optitrack_ekf/src/ekf_node.cpp
@@ -0,0 +1,192 @@
+/* This file is part of the the TVC drone project (https://github.com/EPFLRocketTeam/tvc_drone).
+ *
+ * Copyright (C) 2021  Raphaël Linsen
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+
+#include "ros/ros.h"
+
+#include "geometry_msgs/PoseStamped.h"
+#include "nav_msgs/Odometry.h"
+
+#include <std_msgs/Float64.h>
+
+#include "extended_kalman_filter.hpp"
+#include "real_time_simulator/StateStamped.h"
+#include <tf/transform_broadcaster.h>
+
+class ExtendedKalmanFilterNode {
+private:
+    ExtendedKalmanFilter ekf;
+
+    geometry_msgs::PoseStamped optitrack_pose;
+    bool received_optitrack = false;
+    bool initialized_optitrack = false;
+    Eigen::Vector3d initial_optitrack_position;
+    double last_predict_time;
+    double last_computation_time = 0;
+
+    ros::Publisher ekf_pub;
+    ros::Subscriber optitrack_sub;
+    ros::Publisher computation_time_pub;
+public:
+    double period;
+
+    ExtendedKalmanFilterNode(ros::NodeHandle &nh) : ekf() {
+        // init publishers and subscribers
+        initTopics(nh);
+
+        double pos_std, orientation_std, vel_std, ang_vel_std, optitrack_pos_std, optitrack_orientation_std;
+        if (nh.getParam("predict_std/position", pos_std) &&
+            nh.getParam("predict_std/orientation", orientation_std) &&
+            nh.getParam("predict_std/vel", vel_std) &&
+            nh.getParam("predict_std/angular_vel", ang_vel_std) &&
+            nh.getParam("update_std/position", optitrack_pos_std) &&
+            nh.getParam("update_std/orientation", optitrack_orientation_std)) {
+
+            ExtendedKalmanFilter::state predict_std;
+            predict_std << pos_std, pos_std, pos_std,
+                    orientation_std, orientation_std, orientation_std, orientation_std,
+                    vel_std, vel_std, vel_std,
+                    ang_vel_std, ang_vel_std, ang_vel_std;
+
+            ExtendedKalmanFilter::state_matrix Q;
+            Q.setZero();
+            Q.diagonal() << predict_std.cwiseProduct(predict_std);
+
+            ExtendedKalmanFilter::sensor_data update_std;
+            update_std << optitrack_pos_std, optitrack_pos_std, optitrack_pos_std,
+                    optitrack_orientation_std, optitrack_orientation_std, optitrack_orientation_std, optitrack_orientation_std;
+
+            ExtendedKalmanFilter::sensor_matrix R;
+            R.setZero();
+            R.diagonal() << update_std.cwiseProduct(update_std);
+
+            ExtendedKalmanFilter::state X0;
+            X0 << 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0;
+
+            ekf.init(Q, R, X0);
+
+            double frequency;
+            nh.param<double>("frequency", frequency, 100);
+            period = 1 / frequency;
+
+            initial_optitrack_position << 0, 0, 0;
+
+            last_predict_time = ros::Time::now().toSec();
+        } else {
+            ROS_ERROR("Failed to get kalman filter parameter");
+        }
+    }
+
+    void initTopics(ros::NodeHandle &nh) {
+        ekf_pub = nh.advertise<nav_msgs::Odometry>("/optitrack_state", 1);
+        optitrack_sub = nh.subscribe("/optitrack_client/car/pose", 1,
+                                     &ExtendedKalmanFilterNode::optitrackCallback,
+                                     this);
+        computation_time_pub = nh.advertise<std_msgs::Float64>("debug/computation_time", 10);
+    }
+
+
+    void kalmanStep() {
+        if (received_optitrack) {
+            if (!initialized_optitrack) {
+                initial_optitrack_position = Eigen::Vector3d(optitrack_pose.pose.position.x,
+                                                             optitrack_pose.pose.position.y,
+                                                             optitrack_pose.pose.position.z);
+                initialized_optitrack = true;
+            }
+
+            double compute_time_start = ros::Time::now().toSec();
+
+            Eigen::Quaterniond orientation(optitrack_pose.pose.orientation.w, optitrack_pose.pose.orientation.x,
+                                           optitrack_pose.pose.orientation.y, optitrack_pose.pose.orientation.z);
+            Eigen::Vector3d raw_position(optitrack_pose.pose.position.x, optitrack_pose.pose.position.y,
+                                         optitrack_pose.pose.position.z);
+            Eigen::Vector3d position = raw_position - initial_optitrack_position;
+
+            ExtendedKalmanFilter::sensor_data new_data;
+            new_data.segment(0, 3) = position;
+            new_data.segment(3, 4) = orientation.coeffs();
+
+            double time_now = ros::Time::now().toSec();
+            double dT = time_now - last_predict_time;
+            last_predict_time = time_now;
+
+            ekf.predictStep(dT);
+            ekf.updateStep(new_data);
+
+            publishState();
+
+            last_computation_time = (ros::Time::now().toSec() - compute_time_start) * 1000;
+        }
+    }
+
+    void optitrackCallback(const geometry_msgs::PoseStamped::ConstPtr &pose) {
+        optitrack_pose = *pose;
+        received_optitrack = true;
+    }
+
+
+    void publishState() {
+        real_time_simulator::StateStamped ekf_state_msg;
+        geometry_msgs::Pose pose_msg;
+        geometry_msgs::Twist twist_msg;
+
+        ExtendedKalmanFilter::state X = ekf.getState();
+
+        pose_msg.position.x = X(0);
+        pose_msg.position.y = X(1);
+        pose_msg.position.z = X(2);
+
+        pose_msg.orientation.x = X(3);
+        pose_msg.orientation.y = X(4);
+        pose_msg.orientation.z = X(5);
+        pose_msg.orientation.w = X(6);
+
+        Vector3d vel;
+        vel << X(7),  X(8), X(9);
+        Quaterniond orientation;
+        orientation.coeffs() << X(3), X(4), X(5), X(6);
+        Vector3d vel_body = orientation.inverse()._transformVector(vel);
+
+        twist_msg.linear.x = vel_body(0);
+        twist_msg.linear.y = vel_body(1);
+        twist_msg.linear.z = vel_body(2);
+
+        twist_msg.angular.x = X(10);
+        twist_msg.angular.y = X(11);
+        twist_msg.angular.z = X(12);
+
+        ekf_state_msg.header.stamp = ros::Time::now();
+        ekf_state_msg.state.pose = pose_msg;
+        ekf_state_msg.state.twist = twist_msg;
+        ekf_state_msg.state.propeller_mass = 0;
+
+        ekf_pub.publish(ekf_state_msg);
+
+        std_msgs::Float64 msg3;
+        msg3.data = last_computation_time;
+        computation_time_pub.publish(msg3);
+    }
+
+};
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "optitrack_ekf");
+    ros::NodeHandle nh("optitrack_ekf");
+
+    ExtendedKalmanFilterNode ekf_node(nh);
+
+    // Thread to compute kalman. Duration defines interval time in seconds
+    ros::Timer control_thread = nh.createTimer(ros::Duration(ekf_node.period), [&](const ros::TimerEvent &) {
+        ekf_node.kalmanStep();
+    });
+
+    // Automatic callback of service and publisher from here
+    ros::spin();
+}
---
+++ optitrack_ekf/src/extended_kalman_filter.hpp
@@ -0,0 +1,188 @@
+/* This file is part of the the TVC drone project (https://github.com/EPFLRocketTeam/tvc_drone).
+ *
+ * Copyright (C) 2021  Raphaël Linsen
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+
+#ifndef EXTENDED_KALMAN_FILTER_HPP
+#define EXTENDED_KALMAN_FILTER_HPP
+
+#include "ros/ros.h"
+
+#include "Eigen/Core"
+#include "Eigen/Geometry"
+
+#include "AutoDiffScalar.h"
+
+using namespace Eigen;
+
+class ExtendedKalmanFilter {
+public:
+    static const int NX = 13;
+    static const int NZ = 7;
+
+    // template variables for Autodiff to compute F and H
+    // Autodiff for state
+    template<typename scalar_t>
+    using state_t = Matrix<scalar_t, NX, 1>;
+    using state = state_t<double>;
+    // Autodiff state variable
+    using ad_state = state_t<AutoDiffScalar<state>>;
+
+    // Autodiff for sensor
+    template<typename scalar_t>
+    using sensor_data_t = Matrix<scalar_t, NZ, 1>;
+    using sensor_data = sensor_data_t<double>;
+    // Autodiff sensor variable
+    using ad_sensor_data = sensor_data_t<AutoDiffScalar<state_t<double>>>;
+
+    typedef Matrix<double, NX, NX> state_matrix;
+    typedef Matrix<double, NZ, NZ> sensor_matrix;
+
+private:
+    // EKF usual matrices
+    state_matrix Q; // constant
+    sensor_matrix R; // constant
+    state_matrix F; // computed using autodiff
+    Matrix<double, NZ, NX> H; // computed using autodiff
+
+    state X0;
+    state X;
+    state_matrix P;
+    ad_state ADx;
+
+public:
+    ExtendedKalmanFilter(){
+        Q.setIdentity();
+        R.setIdentity();
+        X.setZero();
+        H.setZero();
+
+        // initialize derivatives ->  the Jacobian of ADx (=X but in autodiff format) is the identity matrix
+        ADx << X;
+        int div_size = ADx.size();
+        int derivative_idx = 0;
+        for (int i = 0; i < ADx.size(); ++i) {
+            ADx(i).derivatives() = state::Unit(div_size, derivative_idx);
+            derivative_idx++;
+        }
+    }
+
+    void init(state_matrix &Q_, sensor_matrix &R_, state &X0){
+        Q = Q_;
+        R = R_;
+
+        P = Q;
+        X = X0;
+    }
+
+    state getState() {
+        return X;
+    }
+
+    template<typename T>
+    void stateDynamics(const state_t<T> &x, state_t<T> &xdot) {
+        // use 3d rigid body dynamics
+        Eigen::Quaternion<T> attitude(x(6), x(3), x(4), x(5));
+        attitude.normalize();
+        // Angular velocity omega in quaternion format to compute quaternion derivative
+        Eigen::Quaternion<T> omega_quat(0.0, x(10), x(11), x(12));
+
+        //state derivatives
+        xdot.segment(0, 3) = x.segment(7, 3);
+        xdot.segment(3, 4) = 0.5 * (attitude * omega_quat).coeffs();
+        xdot.segment(7, 3) << 0.0, 0.0, 0.0;
+        xdot.segment(10, 3) << 0.0, 0.0, 0.0;
+    }
+
+    template<typename T>
+    void measurementModel(const state_t <T> &x, sensor_data_t <T> &z) {
+        z.segment(0, 3) = x.segment(0, 3);
+        z.segment(3, 4) = x.segment(3, 4);
+    }
+
+    void fullDerivative(const state &x,
+                        const state_matrix &P,
+                        state &xdot,
+                        state_matrix &Pdot) {
+        //X derivative
+        stateDynamics(x, xdot);
+
+        //P derivative
+        //propagate xdot autodiff scalar at current x
+        ADx = X;
+        ad_state Xdot;
+        stateDynamics(ADx, Xdot);
+
+        // fetch the jacobian of f(x)
+        for (int i = 0; i < Xdot.size(); i++) {
+            F.row(i) = Xdot(i).derivatives();
+        }
+
+        Pdot = F * P + P * F.transpose() + Q;
+    }
+
+
+    void RK4(const state &X,
+             const state_matrix &P,
+             double dT,
+             state &Xnext,
+             state_matrix &Pnext) {
+        state k1, k2, k3, k4;
+        state_matrix k1_P, k2_P, k3_P, k4_P;
+
+        fullDerivative(X, P, k1, k1_P);
+        fullDerivative(X + k1 * dT / 2, P + k1_P * dT / 2, k2, k2_P);
+        fullDerivative(X + k2 * dT / 2, P + k2_P * dT / 2, k3, k3_P);
+        fullDerivative(X + k3 * dT, P + k3_P * dT, k4, k4_P);
+
+        Xnext = X + (k1 + 2 * k2 + 2 * k3 + k4) * dT / 6;
+        Pnext = P + (k1_P + 2 * k2_P + 2 * k3_P + k4_P) * dT / 6;
+    }
+
+
+    void predictStep(double dT) {
+        //predict: integrate X and P
+        RK4(X, P, dT, X, P);
+    }
+
+    void updateStep(sensor_data &z) {
+        //propagate hdot autodiff scalar at current x
+        ADx = X;
+        ad_sensor_data hdot;
+        measurementModel(ADx, hdot);
+
+        //compute h(x)
+        sensor_data h_x;
+        measurementModel(X, h_x);
+
+        // obtain the jacobian of h(x)
+        for (int i = 0; i < hdot.size(); i++) {
+            H.row(i) = hdot(i).derivatives();
+        }
+
+        // taken from https://github.com/LA-EPFL/yakf/blob/master/ExtendedKalmanFilter.h
+        // this is a faster version of the commented code below
+        Eigen::Matrix<double, NX, NX> IKH;  // temporary matrix
+        Eigen::Matrix<double, NZ, NZ> S; // innovation covariance
+        Eigen::Matrix<double, NX, NZ> K; // Kalman gain
+        Eigen::Matrix<double, NX, NX> I; // identity
+        I.setIdentity();
+        S = H * P * H.transpose() + R;
+        K = S.llt().solve(H * P).transpose();
+        X = X + K * (z - h_x);
+        IKH = (I - K * H);
+        P = IKH * P * IKH.transpose() + K * R * K.transpose();
+
+        // equivalent code:
+        // Matrix<double, NX, NZ> K;
+        // K = P * H.transpose() * ((H * P * H.transpose() + R).inverse());
+        // X = X + K * (z - h_x);
+        // P = P - K * H * P;
+    }
+};
+
+#endif
